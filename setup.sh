86b80d6c2975cee0e8ca26402f3f83ba41490fb7
Vivian Ajay Monis <monis256@gmail.com>
2023-12-19 10:50:08 -0500

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 # Git Setup
-echo "Copying gitconfig to ~/.gitconfig..."
-cp .gitconfig ~/.gitconfig
-
-echo "Configuring git author name and email..."
-git config --global user.name "$GIT_AUTHOR_NAME"
-git config --global user.email "$GIT_AUTHOR_EMAIL"
-
-# Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions 
-
-# Install zsh-syntax
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo "Configuring GIT..."
+cp .gitconfig ${HOME}/.gitconfig
+read -p 'Author Name: ' AUTHOR_NAME
+read -p 'Author Email: ' AUTHOR_EMAIL
+git config --global user.name "$AUTHOR_NAME"
+git config --global user.email "$AUTHOR_EMAIL"
 
 # Alias Setup
-if [ -d "$ZSH_CUSTOM" ]; then
-  echo "Copying aliases to ${ZSH_CUSTOM}/aliases.zsh..."
-  cp aliases.zsh ${ZSH_CUSTOM}
-else
-  echo "${ZSH_CUSTOM} is not setup. Install oh-my-zsh and continue"
-fi
+echo "Configuring Aliases..."
+cp .aliases ${HOME}/.aliases
+
+## TO BE WORKED
+# # Install oh my zsh
+# echo "Installing zsh..."
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# # # Install powerlevel10k
+# # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# # # Install zsh-autosuggestions
+# # git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions 
+
+# # # Install zsh-syntax
+# # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
